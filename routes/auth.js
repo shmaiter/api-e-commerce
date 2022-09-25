e17da57a6084fc2a89da7fa6f7ed9ef25b7f34b6
Shmaither <shmaither@gmail.com>
2022-09-25 11:11:41 -0600

    (feature): Added CryptoJS to encrypt user passwords

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const User = require("../models/User");
-
+const CryptoJS = require("crypto-js");
 // REGISTER
 router.post("/register", async (req, res) => {
   // new User builds by the mongo schema from models
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
   });
 
   try {
