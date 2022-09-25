1a7d1b598263c5262d0333bd4fbb86c2149dee1e
Shmaither <shmaither@gmail.com>
2022-09-25 12:03:50 -0600

    (feature) Login: Validate client password agains decrypt db password

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
-// REGISTER
+
+//
+// .............. REGISTER ..............
+//
 router.post("/register", async (req, res) => {
   // new User builds by the mongo schema from models
   const newUser = new User({
@@ -18,6 +21,32 @@ router.post("/register", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+//
+// .............. lOGIN ..............
+//
+router.post("/login", async (req, res) => {
+  try {
+    // Validate the user exists in mongo database
+    const user = await User.findOne({ username: req.body.username });
+    console.log(user);
+    !user && res.status(401).json("Wrong credentials");
+
+    // const dbHashedPassword = user.password;
+    // Decrypt the hash into a sequence of numbers
+    const dbHashPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+    // Parse the sequence of numbers into the actual user password
+    const password = dbHashPassword.toString(CryptoJS.enc.Utf8);
+    console.log(password);
+    // const loginHashedPassword = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
+    // console.log("loginHashedPassword: " + loginHashedPassword);
+    // console.log("dbHashedPassword: " + dbHashedPassword);
+
+    password !== req.body.password && res.status(401).json("Wrong credentials");
+
+    res.status(200).json(user);
+  } catch (err) {}
 });
 
 module.exports = router;
