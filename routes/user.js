174358f84a78d36408a920bc0ecf3fb55a4277d3
Shmaither <shmaither@gmail.com>
2022-09-26 14:55:58 -0600

    (routes) User: Added delete user endpoint

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 
 const router = require("express").Router();
 
@@ -22,5 +22,28 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// .............. DELETE ..............
+router.delete("/find/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// // .............. GET USER ..............
+// router.get("/:id", verifyTokenAndAdmin, async (req, res) {
+//   try {
+//     const user = await User.findById(req.params.id);
+//     const { password, ...others } = user._doc;
+
+//     res.status(200).json({ ...others });
+//   } catch (err) {
+//     res.status(500).json(err)
+//   }
+// });
 
 module.exports = router;
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -4,9 +4,9 @@ const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.JWT_SEC, (err, userToken) => {
+    jwt.verify(token, process.env.JWT_SEC, (err, payload) => {
       if (err) res.status(403).json("Token is not valid!");
-      req.user = userToken;
+      req.user = payload;
       next();
     });
   } else {
@@ -24,4 +24,14 @@ const verifyTokenAndAuthorization = (req, res, next) => {
   });
 };
 
-module.exports = { verifyToken, verifyTokenAndAuthorization };
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed to do that!");
+    }
+  });
+};
+
+module.exports = { verifyTokenAndAuthorization, verifyTokenAndAdmin };
