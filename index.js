b53584643c35129f4d93e4d1acdb596f854d72f4
Shmaither <shmaither@gmail.com>
2022-10-01 11:12:45 -0600

    feature: add 'order' and 'cart' endpoints to route's carpet

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dontenv = require("dotenv");
 const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 const authRoute = require("./routes/auth");
 
 dontenv.config();
@@ -19,6 +21,8 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running");
--- routes/product.js
+++ routes/product.js
@@ -16,21 +16,20 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
 });
 
 // .............. UPDATE ..............
-// router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
-
-//     try {
-//       const updatedProduct = await Product.findByIdAndUpdate(
-//         req.params.id,
-//         {
-//           $set: req.body,
-//         },
-//         { new: true }
-//       );
-//       res.status(200).json(updatedProduct);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // .............. DELETE ..............
 router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
@@ -74,30 +73,5 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-//   // .............. GET USER STATS  ..............
-//   router.get("/stats/month", verifyTokenAndAdmin, async (req, res) => {
-//     const date = new Date();
-//     const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-//     try {
-//       const data = await User.aggregate([
-//         { $match: { createdAt: { $gte: lastYear } } },
-//         {
-//           $project: {
-//             month: { $month: "$createdAt" },
-//           },
-//         },
-//         {
-//           $group: {
-//             _id: "$month",
-//             total: { $sum: 1 },
-//           },
-//         },
-//       ]);
-//       res.status(200).json(data);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
 
 module.exports = router;
