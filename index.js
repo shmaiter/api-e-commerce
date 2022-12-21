986604ebf718ece612e3c86c1bc7c6f3ce9639cd
Shmaither <shmaither@gmail.com>
2022-12-21 16:06:15 -0600

    feature: add filters to product list page

--- index.js
+++ index.js
@@ -2,28 +2,35 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dontenv = require("dotenv");
+dontenv.config();
 const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const authRoute = require("./routes/auth");
-
-dontenv.config();
-
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB Connection Successfull"))
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DB Connection Successfull"))
+    .catch((err) => {
+        console.log(err);
+    });
 
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    })
+);
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Backend server is running");
+app.listen(PORT, () => {
+    console.log(`Backend server is running on port ${PORT}`);
 });
--- routes/stripe.js
+++ routes/stripe.js
@@ -1,22 +1,54 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_KEY);
 
-router.post("/payment", (req, res) => {
-  stripe.charges.charge(
-    {
-      // Payment id
-      source: req.body.tokenId,
-      amount: req.body.amount,
-      currency: "usd",
-    },
-    (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        res.status(500).json(stripeErr);
-      } else {
-        res.status(200).json(stripeRes);
-      }
+// this represents the data stored in MongoDB
+const storeItems = new Map([
+    [1, { priceInCents: 10000, name: "Learn React Today" }],
+    [2, { priceInCents: 20000, name: "Learn CSS Today" }],
+]);
+
+router.post("/create-checkout-session", async (req, res) => {
+    try {
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            mode: "payment",
+            line_items: req.body.items.map((item) => {
+                const storeItem = storeItems.get(item.id);
+                return {
+                    price_data: {
+                        currency: "usd",
+                        product_data: {
+                            name: storeItem.name,
+                        },
+                        unit_amount: storeItem.priceInCents,
+                    },
+                    quantity: item.quantity,
+                };
+            }),
+            success_url: `http://localhost:3000/success`,
+            cancel_url: `http://localhost:3000/`,
+        });
+
+        res.send({ url: session.url });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
-  );
+
+    // stripe.charges.create(
+    //   {
+    //     source: req.body.tokenId,
+    //     amount: req.body.amount,
+    //     currency: "usd",
+    //   },
+    //   (stripeErr, stripeRes) => {
+    //     if (stripeErr) {
+    //       res.status(500).json(stripeErr);
+    //     } else {
+    //       res.status(200).json(stripeRes);
+    //     }
+    //   }
+    // );
 });
 
-modoule.export = router;
+module.exports = router;
